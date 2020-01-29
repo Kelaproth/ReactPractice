b15c8f0c6fa077f6358b3fd1ab5a41df0ca82ba8
Kevin TANG <tth502025390@gmail.com>
2020-01-29 11:15:07 +0800

    Change the structure to presentational and container components and optimized some code

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -26,15 +26,10 @@ class DishDetail extends Component {
     renderComments(comments) {
         const commentList = comments.map((comment) => {
 
-            var date = new Date(comment.date);
-            var month = date.toDateString().split(' ')[1];
-            var year = date.getFullYear();
-            var day = date.toDateString().split(' ')[2];
-
             return (
                 <div key={comment.id}>
                     <p>{comment.comment}</p>
-                    <p>-- {comment.author}, {month} {day}, {year}</p>
+                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                 </div>
             );
         });
@@ -53,25 +48,29 @@ class DishDetail extends Component {
     render() {
         if(this.props.dish == null){
             return (
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderDish(this.props.dish)}
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                        </div>
                     </div>
                 </div>
             );
         }
         else{
             return (
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderDish(this.props.dish)}
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderComments(this.props.dish.comments)}
+                        </div>
                     </div>
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.dish.comments)}
-                    </div>
-                </div>
+                </div>    
             );
         }
     }
