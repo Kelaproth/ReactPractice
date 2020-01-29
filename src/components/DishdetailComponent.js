cf03e79117ae80d69feb6d36afcf027e382e501d
Kevin TANG <tth502025390@gmail.com>
2020-01-29 20:09:41 +0800

    changed some components to functional components and add header and footer to the application

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,78 +1,67 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
-class DishDetail extends Component {
 
-    renderDish(dish) {
-        if (dish != null) {
-            return(
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else {
-            return(
-                <div></div>
-            );
-        }
+function RenderDish({dish}) {
+    if (dish != null) {
+        return(
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
     }
+    else {
+        return(
+            <div></div>
+        );
+    }
+}
 
-    renderComments(comments) {
-        const commentList = comments.map((comment) => {
-
-            return (
-                <div key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                </div>
-            );
-        });
-
+function RenderComments({comments}) {
+    const commentList = comments.map((comment) => {
         return (
-            <div>
-                <h4>Comments</h4>
-                <ul className="list-unstyled">
-                    <li>{commentList}</li>
-                </ul>
+            <div key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
             </div>
         );
-        
-    }
+    });
 
-    render() {
-        if(this.props.dish == null){
-            return (
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            {this.renderDish(this.props.dish)}
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                        </div>
+    return (
+        <div>
+            <h4>Comments</h4>
+            <ul className="list-unstyled">
+                <li>{commentList}</li>
+            </ul>
+        </div>
+    );
+    
+}
+
+const DishDetail = (props) => {        
+    if (props.dish != null) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />   
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.dish.comments} />
                     </div>
                 </div>
-            );
-        }
-        else{
-            return (
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            {this.renderDish(this.props.dish)}
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            {this.renderComments(this.props.dish.comments)}
-                        </div>
-                    </div>
-                </div>    
-            );
-        }
+            </div>    
+        );
+    }
+    else {
+        return (
+            <div></div>
+        );
     }
 }
 
