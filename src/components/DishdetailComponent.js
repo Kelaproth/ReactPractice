6de78f0c5a2a6d72a559fe9b81ee9ca1451fb601
Kevin TANG <tth502025390@gmail.com>
2020-02-08 00:18:13 +0800

    react animation

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,17 +5,23 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function RenderDish({dish}) {
     if (dish != null) {
         return(
-            <Card>
-                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform in 
+            transformProps={{
+                exitTransform: 'scale(0.5) translateY(-50%)'
+            }}>
+                <Card>
+                    <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         );
     }
     else {
@@ -39,7 +45,11 @@ function RenderComments({comments, postComment, dishId}) {
         <div>
             <h4>Comments</h4>
             <ul className="list-unstyled">
-                <li>{commentList}</li>
+                <Stagger in>
+                    <Fade in>
+                        <li>{commentList}</li>
+                    </Fade>
+                </Stagger>
             </ul>
             <CommentForm dishId={dishId} postComment={postComment} />
         </div>
