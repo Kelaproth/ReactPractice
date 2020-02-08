a60bf0313d0606aa3ef9a7e70095188f15c72827
Kevin TANG <tth502025390@gmail.com>
2020-02-08 13:38:41 +0800

    Assignment 4 part 1 & 2

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -47,7 +47,8 @@ function Home(props) {
                     errMess={props.promoserrMess}/>
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.leader} />
+                    <RenderCard item={props.leader} isLoading={props.leadersLoading}
+                    errMess={props.leaderserrMess}/>
                 </div>
             </div>
         </div>
